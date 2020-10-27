93f6dc3e1be51b78a494c6a1895f7b8ada28e3ee
smccrudden <70445252+smccrudden@users.noreply.github.com>
2020-10-27 12:16:59 -0500

    Add files via upload

---
+++ FindPiThread.cs
@@ -0,0 +1,38 @@
+﻿/* Author:Seth McCrudden
+* Last date modified: 74-Oct-2020
+* File name: FindPiThread.cs
+* Description: Contains dart functions.
+*/
+
+using System;
+
+namespace Project
+{
+    class FindPiThread
+    {
+        private int numOfDarts;
+        public int dartsInCircle { get; private set;}
+        private Random rnd;
+
+        public FindPiThread(int totalDarts)
+        {
+            numOfDarts = totalDarts;
+            dartsInCircle = 0;
+            rnd = new Random();
+        }
+
+        public void throwDarts()
+        {
+            for (int i = 0; i < numOfDarts; i++)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+
+                if ((x * x) + (y * y) <= 1)
+                {
+                    dartsInCircle++;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿/* Author:Seth McCrudden
+* Last date modified: 27-Oct-2020
+* File name: Program.cs
+* Description: Holds main function
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int sleepBetweenThreads = 16;
+
+            Console.Write("Number of throws per thread: ");
+            int throwsCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of threads: ");
+            int threadCount = Convert.ToInt32(Console.ReadLine());
+
+            List<Thread> threads = new List<Thread>(threadCount);
+            List<FindPiThread> findPiThreads = new List<FindPiThread>(threadCount);
+
+            var stopWatch = Stopwatch.StartNew();
+            for (int i = 0; i < threadCount; i++)
+            {
+                findPiThreads.Add(new FindPiThread(throwsCount));
+                threads.Add(new Thread(new ThreadStart(findPiThreads[i].throwDarts)));
+                threads[i].Start();
+                Thread.Sleep(sleepBetweenThreads);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+
+            long dartsInCircle = 0;
+            long dartsThrown = (long)throwsCount * threadCount;
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                dartsInCircle += findPiThreads[i].dartsInCircle;
+            }
+
+            double PI = (double)(4 * dartsInCircle) / dartsThrown;
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Took " + (stopWatch.ElapsedMilliseconds - sleepBetweenThreads * threadCount) + " ms");
+            Console.WriteLine("Total Darts Inside Circle: " + dartsInCircle);
+            Console.WriteLine("Total Darts Thrown: " + dartsThrown);
+            Console.WriteLine("PI: " + PI);
+
+            Console.ReadKey();
+        }
+    }
+}
